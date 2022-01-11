cb26aec47e8ed4e1865a8735a2c48b1fc5ed68a4
delsonb <delson@insiderstore.com.br>
2022-01-11 17:17:30 -0300

    add new stream invoices to steams.py

--- tap_bling_erp/streams.py
+++ tap_bling_erp/streams.py
@@ -86,7 +86,64 @@ class Orders(Stream):
         
         return state
 
+class Invoices(Stream):
+    tap_stream_id = 'bling_invoices'
+    key_properties = ['id'] # What's the role of this field?
+    replication_method = 'INCREMENTAL'
+    valid_replication_keys = ['dataEmissao']
+    replication_key = 'dataEmissao'
+
+    # Invoices come from API as tz America/Sao_Paulo, but without the tz info in the payload. Therefore Singer will append UTC 0 by default, but the datetime value will still represent America/Sao_Paulo. This has to be converted in the DW later.
+    def sync(self, state, stream_schema, stream_metadata, config, transformer):
+        # Bookmark datetime is tz America/Sao_Paulo
+        start_time_str = singer.get_bookmark(
+            state,
+            self.tap_stream_id,
+            self.replication_key,
+            config['start_date'])
+        # add timezone UTC 0 without changing the datetime
+        start_time = datetime.strptime(start_time_str,BOOKMARK_DATE_FORMAT).replace(tzinfo=pytz.UTC)
+        max_record_value = start_time
+
+        # singer.utils.now brings time in UTC converting correctly from the OS
+        singer_now = singer.utils.now()
+        # It's necessary to convert to the API tz
+        extraction_time = singer_now.astimezone(pytz.timezone(self.sao_paulo_tz_fixed(singer_now)))
+        api_start_time = datetime.strftime(start_time,API_REQ_DATETIME_FORMAT)
+
+        # To ensure data quality truncate to use second 0 and use finish date as 2 minutes ago
+        api_finish_time = extraction_time.replace(second=0,microsecond=0) - timedelta(minutes=2)
+        api_finish_time = datetime.strftime(api_finish_time,API_REQ_DATETIME_FORMAT)
+        
+        # get invoices from API and iterate over results
+        for page, records in self.client.invoices(api_start_time,api_finish_time):
+            for record in records:
+                record = extract_last_updated(record['notafiscal'],API_RESP_DATETIME_FORMAT)
+                transformed_record = transformer.transform(record, stream_schema, stream_metadata)
+
+                # as the transformed_record returns any datetime field as a str in the format of "%04Y-%m-%dT%H:%M:%S.%fZ", it's necessary to convert it to datetime for comparisons.
+                # replace method is used because data was only faked as if it was UTC 0 timezone, but no changes in datetime were applied, so we don't need to change it again
+                updated_at = datetime.strptime(
+                    transformed_record[self.replication_key],
+                    BOOKMARK_DATE_FORMAT)
+                updated_at = updated_at.replace(tzinfo=pytz.timezone(self.sao_paulo_tz_fixed(updated_at)))
+
+                singer.write_record(self.tap_stream_id,transformed_record,time_extracted=extraction_time)
+
+                if updated_at > max_record_value:
+                    max_record_value = updated_at
+
+        # As the API will return data where updated_at < HH:MM, use the next minute as the bookmark
+        # Altough updated_at is always truncated at the minute level, does this manual truncation just to be sure future updates in the API won't break the code
+        max_record_value = max_record_value + timedelta(minutes=1)
+        max_record_value = datetime.strftime(
+            max_record_value.replace(second=0),BOOKMARK_DATE_FORMAT)
+        state = singer.write_bookmark(state, self.tap_stream_id, self.replication_key, max_record_value)
+        singer.write_state(state)
+        
+        return state
 
 STREAMS = {
-    'bling_orders': Orders
+    'bling_orders': Orders,
+    'bling_invoices': Invoices
 }
